914e3e8d0d625f94e788cc668352429aa2a9c7e0
Zachary Baca <zachary.baca@msn.com>
2024-05-04 12:36:32 -0500

    completed python-naming-practice

---
+++ python-naming-convention-practice.py
@@ -0,0 +1,23 @@
+
+# Task 1: Code Correction
+ # You have been given a piece of code with several variable and constant names that don't follow the Python naming convention. Your task is to correct them:
+
+PI_VALUE = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+
+
+"""Task 1: Code Correction
+Given below are some variable assignments. Your task is to identify the data type of each variable and then use the type() function to print it out:"""
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
