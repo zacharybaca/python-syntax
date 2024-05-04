9207b4fab048d9144cdd6498542d0e7b5fb9700c
Zachary Baca <zachary.baca@msn.com>
2024-05-04 12:53:38 -0500

    completed understanding-assignments-comparisons

---
+++ understanding-assignments-and-comparisons.py
@@ -0,0 +1,13 @@
+
+"""Task 1: Value Swapping
+Swap the values of two given numbers using assignment operators (=) and then compare them to ensure they have been swapped."""
+
+number_one = 1
+number_two = 2
+placeholder_number = 0
+
+placeholder_number = number_one
+number_one = number_two
+number_two = placeholder_number
+
+print(number_two > number_one)
